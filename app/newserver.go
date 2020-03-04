765d56e0c82b783704fca3f29d07a5994a194eeb
Alexander Wurts <ajwurts@gmail.com>
2020-03-04 18:44:58 -0500

    Boiler Plate

---
+++ app/newserver.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/ajwurts/autodb/developer"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Println(r.URL.User)
+	fmt.Println("Host", r.URL.Host)
+	fmt.Println("Path", r.URL.Path)
+	fmt.Println("RawPath", r.URL.RawPath)
+	fmt.Println("RawQuery", r.URL.RawQuery)
+	fmt.Println("Fragment", r.URL.Fragment)
+
+}
+
+
+func main() {
+
+	user := developer.User{Username:"ajwurts", Password: "password", Token: "token"}
+	fmt.Println(user)
+
+	fmt.Println("Create Account: ", developer.CreateAccount("Ajwurts", "password"))
+
+	// Failed Login
+	token, err := developer.Login("Ajwurts", "pasword")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+
+	// Successful Login
+	token, err1 := developer.Login("Ajwurts", "password")
+	if err != nil {
+		fmt.Println(err1)
+	}
+	fmt.Println("Log In: ", token)
+
+	// Get Developer
+	dev, err2 := developer.GetDeveloper(token)
+	if err != nil {
+		fmt.Println(err2)
+	}
+	fmt.Println(dev)
+
+
+	newUser := &developer.User{Username: "Ajwurts", Password: "Password2", Token: token}
+	res, err3 := developer.ModifyDeveloper(newUser)
+	if err3 != nil {
+		fmt.Println(err3)
+	}
+
+	fmt.Println(res)
+
+	// Test Modify Developer
+	userTest, err4 := developer.GetDeveloperByUsername("Ajwurts")
+	if err != nil {
+		fmt.Println(err4)
+	}
+	fmt.Println(userTest)
+}
---
+++ developer/developer.go
@@ -0,0 +1,107 @@
+package developer
+
+import "fmt"
+
+type User struct  {
+	Username string
+	Password string
+	Token string
+	loggedIn bool
+}
+
+type LoginError string 
+
+func (e LoginError) Error() string {
+	return fmt.Sprintf("failed to log in: %s", string(e))
+}
+
+type UserNotFoundError string
+
+func (e UserNotFoundError) Error() string {
+	return fmt.Sprintf("User Not Found: %s", string(e))
+}
+
+
+
+var data map[string]*User = make(map[string]*User)
+var tokenKey map[string]*User = make(map[string]*User)
+
+func generateToken() string {
+	return "12345678910"
+}
+
+func Login(Username, Password string) (string, error) {
+	if usr, ok := data[Username]; ok {
+		if usr.Password == Password && !usr.loggedIn {
+			usr.loggedIn = true
+			token := generateToken()
+			usr.Token = token
+			tokenKey[token] = usr
+			return token, nil
+
+		} else {
+			return "", LoginError("Bad Username or Password")
+		}
+	}
+	return "", LoginError("Bad Username or Password")
+}
+
+func Logout(Username string) bool {
+	if usr, ok := data[Username]; ok {
+		usr.loggedIn = false
+		return true
+	}
+	return false
+}
+
+func CreateAccount(Username, Password string) bool {
+	if _, ok := data[Username]; ok {
+		return false
+	} else {
+		user := &User{Username: Username, Password: Password}
+		data[Username] = user
+		return true
+	}
+
+}
+
+func GetDeveloper(token string) (*User, error) {
+	if usr, ok := tokenKey[token]; ok {
+		return usr, nil
+	} else {
+		return &User{}, UserNotFoundError("No User Connected to Token")
+	}
+}
+
+func GetDeveloperByUsername(username string) (*User, error) {
+	if usr, ok := data[username]; ok {
+		return usr, nil
+	} else {
+		return &User{}, UserNotFoundError("No User Connected to Token")
+	}
+}
+
+
+func ModifyDeveloper(newUser *User) (bool, error) {
+	if usr, ok := tokenKey[newUser.Token]; ok {
+		*usr = *newUser
+		return true, nil
+	}
+
+	return false, UserNotFoundError(newUser.Username)
+}
+
+func DeleteDeveloper(token string) bool {
+	if usr, ok := tokenKey[token]; ok {
+		username := usr.Username
+		delete(tokenKey, token)
+		delete(data, username)
+		return true
+	} else {
+		return false
+	}
+}
+
+func (u User) String() string {
+	return fmt.Sprintf("%s, %s, %s", u.Username, u.Password, u.Token)
+}
