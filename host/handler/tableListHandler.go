40665ae988a60a101da5d5458d7c6a208353f347
xudufy <xudufy@qq.com>
2020-04-04 06:29:46 -0400

    addtable done w/o test

--- host/handler/tableListHandler.go
+++ host/handler/tableListHandler.go
@@ -3,9 +3,12 @@ package handler
 import (
 	"autodb/host/dbconfig"
 	"autodb/host/globalsession"
+	"encoding/json"
 	"html/template"
 	"net/http"
+	"regexp"
 	"strconv"
+	"strings"
 )
 
 type TableListHandler struct {}
@@ -15,7 +18,8 @@ var tableListTmpl *template.Template
 func (*TableListHandler) Init() {
 	tableListTmpl, _ = template.ParseFiles("../view/tables.html")
 	http.HandleFunc("/project", viewTableListHandler)
-	http.HandleFunc("/addTable", addTableHandler)
+	http.HandleFunc("/addTable/", addTableHandler)
+	http.HandleFunc("/deleteTable", deleteTableHandler)
 }
 
 func viewTableListHandler(w http.ResponseWriter, r *http.Request) {
@@ -74,6 +78,194 @@ func viewTableListHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+type ColumnInfo struct {
+	Name string `json:"name"`
+	ColType string `json:"type"`
+	Options string `json:"options"`
+}
+
+type TableInfo struct {
+	Name string `json:"name"`
+	Columns []ColumnInfo `json:"columns"`
+	Options string `json:"options"`
+}
+
+//TODO: test
 func addTableHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method!="POST" {
+		http.NotFound(w, r)
+		return
+	}
+
+	urlPart := strings.Split(r.URL.Path, "/")[1:]
+	if len(urlPart)!=2 || urlPart[1]=="" {
+		http.NotFound(w, r)
+		return
+	}
+
+	pid, err := strconv.Atoi(urlPart[1])
+	if err!=nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	if r.ContentLength > 8192 {
+		NewJSONError("too long", 400, w)
+		return
+	}
+
+	uid, err := globalsession.GetUid(w, r)
+	if err!=nil {
+		NewJSONError("Not Authorized", 403, w)
+		return
+	}
+	group:= globalsession.GetGroupToProject(uid, pid)
+	if (group & (globalsession.UserGroupOwner | globalsession.UserGroupDeveloper)) == 0 {
+		NewJSONError("Not Authorized",403, w)
+		return
+	}
+
+	pname := ""
+	pnameRow, err := dbconfig.HostDB.Query(`select pname from projects where pid=?;`, pid)
+	if err!=nil {
+		NewJSONError(err.Error(), 502, w)
+		return
+	}
+	defer pnameRow.Close()
+	if pnameRow.Next() {
+		_ = pnameRow.Scan(&pname)
+	}
+
+	if pname == "" {
+		NewJSONError("project not found", 502, w)
+		return
+	}
+
+	var form TableInfo
+
+	err = json.NewDecoder(r.Body).Decode(&form)
+	if err!=nil {
+		NewJSONError("json input parse error", 400, w)
+		return
+	}
+
+	//fmt.Println(form)
+
+	if ok, _ := regexp.MatchString(`^[A-Za-z_][\w_]{0,63}$`, form.Name); !ok {
+		NewJSONError("table name invalid", 400, w)
+		return
+	}
+
+	for _, v := range form.Columns {
+		if ok, _ := regexp.MatchString(`^[A-Za-z_][\w_]{0,63}$`, v.Name); !ok {
+			NewJSONError("column name" + v.Name + " invalid", 400, w)
+			return
+		}
+
+		if ok, _ := regexp.MatchString(`^[^,;]$`, v.Options); !ok {
+			NewJSONError("column options should not contain , and ;", 400, w)
+			return
+		}
+
+		if ok := checkColumnType(v.ColType);!ok {
+			NewJSONError( "column type unsupported", 400, w)
+			return
+		}
+	}
+
+	if ok, _ := regexp.MatchString(`^[^'";]*$`, form.Options); !ok {
+		NewJSONError(`table option must not contain ' " and ;`, 400, w)
+		return
+	}
+
+	options := strings.Split(form.Options, ",")
+	for _, vo := range options {
+		v := strings.TrimSpace(vo)
+		flag := (v=="") || strings.HasPrefix(v, "PRIMARY KEY")
+		flag = flag || strings.HasPrefix(v, "INDEX")
+		flag = flag || strings.HasPrefix(v, "UNIQUE")
+		flag = flag || strings.HasPrefix(v, "FOREIGN KEY")
+		if !flag {
+			NewJSONError("table option unsupported: "+ vo, 400, w)
+			return
+		}
+	}
+
+	tx, err := dbconfig.HostDB.Begin()
+	if err!=nil {
+		NewJSONError(err.Error(), 502, w)
+		return
+	}
+	defer func() {
+		if err!=nil {
+			_ = tx.Rollback()
+		}
+	}()
+
+	_, err = tx.Exec(`insert into tables (pid, name) VALUES (?, ?)`, pid, form.Name)
+	if err!=nil {
+		NewJSONError("name has been used", 400, w)
+		return
+	}
+
+	var tableBuildQuery strings.Builder
+	tableBuildQuery.WriteString(`create table ` + form.Name + " (\n")
+
+	for i, v:=range form.Columns {
+		if i!=0 {
+			tableBuildQuery.WriteRune(',')
+		}
+		tableBuildQuery.WriteString(v.Name+" "+ v.ColType + " " + v.Options + "\n")
+	}
+
+	form.Options = strings.TrimSpace(form.Options)
+	if form.Options != "" {
+		tableBuildQuery.WriteString("," + form.Options)
+	}
+
+	tableBuildQuery.WriteString(");")
+
+	dbc, err := dbconfig.GetProjectInternalConn(pid, pname)
+	if err!=nil {
+		NewJSONError(err.Error(), 502, w)
+		return
+	}
+	_, err = dbc.Exec(tableBuildQuery.String())
+	if err!=nil {
+		NewJSONError(err.Error(), 400, w)
+		return
+	}
+
+	err = tx.Commit()
+
+}
+
+func checkColumnType(coltype string) bool {
+	coltype = strings.TrimSpace(coltype)
+	switch coltype {
+	case "MEDIUMTEXT":
+		fallthrough
+	case "INT":
+		fallthrough
+	case "BIGINT":
+		fallthrough
+	case "DOUBLE":
+		fallthrough
+	case "DATETIME":
+		return true
+	}
+
+	if ok, _ := regexp.MatchString(`^VARCHAR\([0-9]{1,5}\)$`, coltype); ok {
+		return true
+	}
+
+	if ok, _ := regexp.MatchString(`^ENUM\(\s*'[\w_ ]+'\s*(,\s*'[\w_ ]+'\s*)+\)$`, coltype); ok {
+		return true
+	}
+
+	return false
+}
+
+func deleteTableHandler(w http.ResponseWriter, r *http.Request) {
 
 }
